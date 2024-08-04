9de493a2f423e297a8db4c3c19d781db33be7c18
Janith CB <42415329+janithcbk123@users.noreply.github.com>
2024-08-04 21:07:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,53 +13,6 @@ st.set_page_config(
 
 # ---------------------------------------------------------------------
 
-mp_drawing = mp.solutions.drawing_utils
-mp_draw= mp.solutions.drawing_utils
-mp_face_mesh = mp.solutions.face_mesh
-mp_draw= mp.solutions.drawing_utils
-mp_hand= mp.solutions.hands
-mp_hands = mp.solutions.hands
-mp_drawing_styles = mp.solutions.drawing_styles
-
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
-    return image, results
-
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
-
-def draw_styled_landmarks(image, results):
-    # Draw face connections
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                             ) 
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                             ) 
-    # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                             ) 
-    # Draw right hand connections  
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                             ) 
-
-
-# ----------------------------------------------------------------------
-
 st.title('📚 Sinhala Sign Language Learning Aid')
 st.info('Sinhala Sign Language Learning Aid Using Machine Learning And Computer Vision')
 
@@ -73,6 +26,8 @@ if st.sidebar.button('Github'):
 st.sidebar.title('Menu')
 st.sidebar.subheader('Settings')
 
+@st.cache()
+
 # ----------------------------------------------------------------------
 
 app_mode= st.sidebar.selectbox('Choose the App Mode',
@@ -81,100 +36,9 @@ app_mode= st.sidebar.selectbox('Choose the App Mode',
 # ----------------------------------------------------------------------
 
 if app_mode== 'Learn Sign Language':
-    st.markdown('Developed by **Thesara_Wiki** & **Janith_C**')
-    st.markdown('App Made using **Mediapipe** & **Open CV**')
-
-    st.sidebar.subheader('Number you want to learn')
-    number = st.sidebar.number_input(
-    "Insert a number", min_value=1, max_value=30, value=None, step=1, placeholder="Type a number..."
-    )
+    
 
 #####################################################################################################################################################################
-    pose = []
-    for res in results.pose_landmarks.landmark:
-        test = np.array([res.x, res.y, res.z, res.visibility])
-        pose.append(test)
-        
-    colors = [(245,117,16), (117,245,16), (16,117,245)]
-    def prob_viz(res, actions, input_frame, colors):
-        output_frame = input_frame.copy()
-        for num, prob in enumerate(res):
-            cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
-            cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-        
-        return output_frame
-
-
-    plt.figure(figsize=(18,18))
-    plt.imshow(prob_viz(res, actions, image, colors))
-
-    sequence.reverse()
-
-    len(sequence)
-
-    sequence.append('def')
-
-    sequence.reverse()
-
-    sequence[-30:]
-
-    # 1. New detection variables
-    sequence = []
-    sentence = []
-    threshold = 0.8
-
-    cap = cv2.VideoCapture(0)
-    # Set mediapipe model 
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while cap.isOpened():
-
-            # Read feed
-            ret, frame = cap.read()
-            
-            # Make detections
-            image, results = mediapipe_detection(frame, holistic)
-            print(results)
-        
-            # Draw landmarks
-            draw_styled_landmarks(image, results)
-        
-            # 2. Prediction logic
-            keypoints = extract_keypoints(results)
-#           sequence.insert(0,keypoints)
-#           sequence = sequence[:30]
-            sequence.append(keypoints)
-            sequence = sequence[-30:]
-        
-            if len(sequence) == 30:
-                res = model.predict(np.expand_dims(sequence, axis=0))[0]
-                print(actions[np.argmax(res)])
-            
-            
-            #3. Viz logic
-                if res[np.argmax(res)] > threshold: 
-                    if len(sentence) > 0: 
-                        if actions[np.argmax(res)] != sentence[-1]:
-                            sentence.append(actions[np.argmax(res)])
-                    else:
-                        sentence.append(actions[np.argmax(res)])
-
-                if len(sentence) > 5: 
-                    sentence = sentence[-5:]
-
-                # Viz probabilities
-                image = prob_viz(res, actions, image, colors)
-            
-            cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
-            cv2.putText(image, ' '.join(sentence), (3,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        
-            # Show to screen
-            cv2.imshow('OpenCV Feed', image)
-
-            # Break gracefully
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-        cap.release()
-
 #####################################################################################################################################################################
     
 # ----------------------------------------------------------------------
