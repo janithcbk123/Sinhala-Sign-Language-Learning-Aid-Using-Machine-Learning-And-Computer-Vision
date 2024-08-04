b8ea555ebdcfd731a14612e3883e6ea9bef10d6c
Janith CB <42415329+janithcbk123@users.noreply.github.com>
2024-08-04 20:15:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,14 @@ mp_hand= mp.solutions.hands
 mp_hands = mp.solutions.hands
 mp_drawing_styles = mp.solutions.drawing_styles
 
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
 # ----------------------------------------------------------------------
 
 st.title('📚 Sinhala Sign Language Learning Aid')
