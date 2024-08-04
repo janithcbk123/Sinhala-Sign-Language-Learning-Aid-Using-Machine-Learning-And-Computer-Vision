84e98e8c0e688ba8a66de708b87267ccc77aeda1
Janith CB <42415329+janithcbk123@users.noreply.github.com>
2024-08-04 10:47:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('Sinhala Sign Language Learning Aid')
+st.title('📚 Sinhala Sign Language Learning Aid')
 
 st.info('Sinhala Sign Language Learning Aid Using Machine Learning And Computer Vision')
