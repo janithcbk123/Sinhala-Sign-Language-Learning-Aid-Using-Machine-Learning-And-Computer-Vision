349bb2392abb5e317f4284c88ea15368b6179c70
Janith CB <42415329+janithcbk123@users.noreply.github.com>
2024-08-04 20:11:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cv2
 import mediapipe as mp
+from matplotlib import pyplot as plt
 
 # ---------------------------------------------------------------------
 st.set_page_config(
