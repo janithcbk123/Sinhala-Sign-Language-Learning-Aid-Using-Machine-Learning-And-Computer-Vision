293c06706d5c7082d2d0d9649cc01a06347c814e
Janith CB <42415329+janithcbk123@users.noreply.github.com>
2024-08-05 07:19:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import mediapipe as mp
+#import mediapipe as mp
 from matplotlib import pyplot as plt
 
 # ---------------------------------------------------------------------
