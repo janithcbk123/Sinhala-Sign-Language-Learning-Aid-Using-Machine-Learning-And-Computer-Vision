7a0f2c11b324f5f448516add0614df1141aa192c
Janith CB <42415329+janithcbk123@users.noreply.github.com>
2024-08-08 15:39:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,16 @@ import streamlit as st
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture(0)
 
 while run:
-    _, frame = camera.read()
+    ret, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    
     FRAME_WINDOW.image(frame)
+    
 else:
     st.write('Stopped')
 
